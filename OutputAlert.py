831acf25becbf7e226429848cc62abd54ef745f1
xhyzzZ <41977460+xhyzzZ@users.noreply.github.com>
2019-02-04 17:32:21 -0500

    Add files via upload

---
+++ OutputAlert.py
@@ -0,0 +1,15 @@
+def receive_basic_iuput_data(parameter);
+	
+	return receivedData;
+
+def send_basic_input_data(parameter);
+
+	return sentData;
+
+def receive_AI_iuput_data(parameter);
+	
+	return receivedData;
+
+def send_AI_input_data(parameter);
+
+	return sentData;
