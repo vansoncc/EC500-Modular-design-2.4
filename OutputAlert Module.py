ad39dbb6b377f3e7dabd51798c8a5c6dac20194b
xhyzzZ <41977460+xhyzzZ@users.noreply.github.com>
2019-02-04 17:39:49 -0500

    Add files via upload

---
+++ OutputAlert Module.py
@@ -0,0 +1,17 @@
+def receive_basic_iuput_data(parameter);
+	##recevie data from input module, then analyze it using some judge functions to generate boolean result
+	##paramter is boolean 
+	return receivedData;
+
+def send_basic_input_data(parameter);
+	##receive the result and show it on terminal or web page
+	return sentData;
+
+def receive_AI_iuput_data(parameter);
+	##recevie AI data from input module, then analyze it using some judge functions to generate boolean result
+	##paramter is boolean 
+	return receivedData;
+
+def send_AI_input_data(parameter);
+	##receive the result and show it on terminal or web page
+	return sentData;
